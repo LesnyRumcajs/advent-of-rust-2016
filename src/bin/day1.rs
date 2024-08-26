d4318760b0330b62dc89c5178c542707eaa5bb98
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2024-08-26 19:56:53 +0200

    feat: scaffolding

---
+++ src/bin/day1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Day 1, part 1: {}", part1());
+    println!("Day 1, part 2: {}", part2());
+}
+
+fn part1() -> i32 {
+    unimplemented!();
+}
+fn part2() -> i32 {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{fs::File, io::BufReader};
+
+    #[test]
+    fn test_solution() {}
+}
